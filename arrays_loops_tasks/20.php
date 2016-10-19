0986d6cef8628f90ddb1f1dcd4b41389036b9d7c
valer-yukhta <v.yukhta@mail.ua>
2016-10-19 17:55:46 +0300

    Update 20.php
    UTC

--- arrays_loops_tasks/20.php
+++ arrays_loops_tasks/20.php
@@ -1,5 +1,5 @@
-<?php
-$day = "Sunday";
+date_default_timezone_set('UTC');
+$day = date("l");
 $week = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
 Foreach ($week as $val){
     if ($val == $day){
